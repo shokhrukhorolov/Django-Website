080ad8ada56b63b2fadebaa76a295616274723df
zander <shoxs5058@gmail.com>
2024-01-19 17:35:18 +0500

    все теперь готово осталось только сделать фронт ввиде medium

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from .views import post_list
+
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', post_list, name='post_list'),
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -2,8 +2,8 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 BOOTSTRAP_ADMIN_SIDEBAR_MENU = False
 
 # Quick-start development settings - unsuitable for production
@@ -46,7 +46,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'my_django_project/templates')],
+        'DIRS': [BASE_DIR / 'blog' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -111,7 +111,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
